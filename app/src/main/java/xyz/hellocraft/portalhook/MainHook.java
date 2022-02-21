1ac1ecdd4c58e92065bf8c10f33360c67deac1a1
Haocen2004 <haocen.minecraft@qq.com>
2022-02-21 09:30:51 +0800

    try create System Hook

--- app/src/main/java/xyz/hellocraft/portalhook/MainHook.java
+++ app/src/main/java/xyz/hellocraft/portalhook/MainHook.java
@@ -4,6 +4,7 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import xyz.hellocraft.portalhook.hook.PortalHook;
 import xyz.hellocraft.portalhook.hook.XiaoAiHook;
+import xyz.hellocraft.portalhook.hook.SystemHook;
 
 public class MainHook implements IXposedHookLoadPackage {
     @Override
@@ -11,8 +12,13 @@ public class MainHook implements IXposedHookLoadPackage {
         switch (lpparam.packageName) {
             case "com.miui.contentextension":
                 new PortalHook(lpparam);
+                break;
             case "com.miui.voiceassist":
                 new XiaoAiHook(lpparam);
+                break;
+            case "android":
+                new SystemHook(lpparam);
+                break;
         }
     }
 }
---
+++ app/src/main/java/xyz/hellocraft/portalhook/hook/SystemHook.java
@@ -0,0 +1,32 @@
+package xyz.hellocraft.PortalHook.hook;
+
+import android.content.Intent;
+import android.content.Context;
+import android.os.Bundle;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class SystemHook {
+
+    public SystemHook(XC_LoadPackage.LoadPackageParam lpparam) {
+        XposedBridge.log("Hook到系统进程！");
+        final Class<?> clazz = XposedHelpers.findClass("android.app.ActivityManager", lpparam.classLoader);
+        //getClassInfo(clazz);
+
+        XposedHelpers.findAndHookMethod(clazz, "startActivity", Context.class, Intent.class,Bundle.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                XposedBridge.log("Hook到方法:"+param.args[1].toString());
+                // Uri uri = Uri.parse(param.args[0].toString());
+                // Intent intent = new Intent();
+                // intent.setAction("android.intent.action.VIEW");
+                // intent.setData(uri);
+                // return intent;
+            }
+        });
+
+    }
+}
